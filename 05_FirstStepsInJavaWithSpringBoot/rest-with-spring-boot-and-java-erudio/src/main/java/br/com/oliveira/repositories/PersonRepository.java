1ae3546d50b4e009b58487e1e80b0e9336e8c58d
João Vitor <joao.vitor@icondev.com.br>
2024-03-11 22:28:05 -0300

    Ajustes da aula 0904

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/oliveira/repositories/PersonRepository.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/oliveira/repositories/PersonRepository.java
@@ -3,8 +3,8 @@ package br.com.oliveira.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.oliveira.model.Person;
+import br.com.oliveira.data.vo.v1.PersonVO;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {
+public interface PersonRepository extends JpaRepository<PersonVO, Long> {
 }
